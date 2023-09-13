c1e97cd00eee13270167deaf1a0a647334bb851d
TaneaT <110297062+TaneaT@users.noreply.github.com>
2023-09-13 13:31:25 +0300

    Finished purchase flow (#4)

---
+++ src/test/java/com/practiceUni/shoppingWeb/dao/impl/PurchaseDAOImplTest.java
@@ -0,0 +1,93 @@
+package com.practiceUni.shoppingWeb.dao.impl;
+
+import com.practiceUni.shoppingWeb.JdbcConnection;
+import com.practiceUni.shoppingWeb.dao.PurchaseDAO;
+import com.practiceUni.shoppingWeb.domain.Purchase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PurchaseDAOImplTest {
+
+  @Autowired private PurchaseDAO purchaseDAO;
+
+  private Purchase getTestPurchase() {
+    return new Purchase(Timestamp.valueOf(LocalDateTime.now()), 32, 141, 39);
+  }
+
+  @AfterEach
+  void tearDown() {
+    String purchaseSql = "DELETE FROM purchase";
+    String userPurchaseSql = "DELETE FROM user_purchase";
+
+    try (Connection conn = JdbcConnection.getConnection();
+        PreparedStatement purchased = conn.prepareStatement(purchaseSql);
+        PreparedStatement userPurchase = conn.prepareStatement(userPurchaseSql)) {
+
+      userPurchase.executeUpdate();
+      purchased.executeUpdate();
+
+    } catch (SQLException e) {
+      e.getStackTrace();
+    }
+  }
+
+  @Test
+  void create() {
+    Purchase purchase = getTestPurchase();
+
+    assertNotNull(purchaseDAO.create(purchase));
+
+    Purchase testPurchase = purchaseDAO.findById(purchase.getPurchaseId());
+    assertNotNull(testPurchase);
+    assertEquals(testPurchase.getPurchaseId(), purchase.getPurchaseId());
+  }
+
+  @Test
+  void deleteById() {
+    Purchase purchase = getTestPurchase();
+    assertNotNull(purchaseDAO.create(purchase));
+
+    assertTrue(purchaseDAO.deleteById(purchase.getPurchaseId()));
+  }
+
+  @Test
+  void findById() {
+    Purchase purchase = getTestPurchase();
+    assertNotNull(purchaseDAO.create(purchase));
+
+    Purchase foundPurchase = purchaseDAO.findById(purchase.getPurchaseId());
+    assertNotNull(foundPurchase);
+    assertEquals(foundPurchase.getPurchaseId(), purchase.getPurchaseId());
+  }
+
+  @Test
+  void findAllById() {
+    Purchase purchase = getTestPurchase();
+    Purchase purchase1 = getTestPurchase();
+
+    assertNotNull(purchaseDAO.create(purchase));
+    assertNotNull(purchaseDAO.create(purchase1));
+
+    List<Purchase> purchases = new ArrayList<>();
+    purchases.add(purchase);
+    purchases.add(purchase1);
+
+    List<Purchase> allPurchases = purchaseDAO.findAllById(purchase.getUserId());
+    assertNotNull(allPurchases);
+    assertEquals(allPurchases.size(), purchases.size());
+  }
+}
